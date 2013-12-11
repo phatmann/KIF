8d1dc220f54f014a41b9a84e627744e3cd78d606
Tony DiPasquale <tony@thoughtbot.com>
2013-12-11 11:01:53 -0500

    Modify TestCase category to use XCTest when included

--- Additions/TestCase-KIFAdditions.m
+++ Additions/TestCase-KIFAdditions.m
@@ -1,24 +1,32 @@
 //
-//  SenTestCase-KIFAdditions.m
+//  TestCase-KIFAdditions.m
 //  KIF
 //
-//  Created by Brian Nickel on 8/23/13.
+//  Created by Tony DiPasquale on 12/9/13.
 //
 //
 
-#import "SenTestCase-KIFAdditions.h"
+#import "TestCase-KIFAdditions.h"
 #import "LoadableCategory.h"
 
-MAKE_CATEGORIES_LOADABLE(SenTestCase_KIFAdditions)
+MAKE_CATEGORIES_LOADABLE(TestCase_KIFAdditions)
 
+#ifdef XCT_EXPORT
+@implementation XCTestCase (KIFAdditions)
+#else
 @implementation SenTestCase (KIFAdditions)
+#endif
 
 - (void)failWithException:(NSException *)exception stopTest:(BOOL)stop
 {
+#ifdef XCT_EXPORT
+    [self recordFailureWithDescription:exception.description inFile:exception.userInfo[@"SenTestFilenameKey"] atLine:[exception.userInfo[@"SenTestLineNumberKey"] unsignedIntegerValue] expected:YES];
+#else
     if (stop) {
         [self raiseAfterFailure];
     }
     [self failWithException:exception];
+#endif
     [self continueAfterFailure];
 }
 
